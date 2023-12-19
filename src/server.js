bdbbe0c136cc2c7cce40f7fc6cd9c7e4a65c3478
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-19 13:46:00 +0200

    last

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-const users = [
-  { id: 1, name: 'James', town: 'London' },
-  { id: 2, name: 'Mike', town: 'Kaunas' },
-  { id: 3, name: 'Bob', town: 'Vilnius' },
-  { id: 4, name: 'Jane', town: 'Klaipeda' },
+let users = [
+  { id: 1, name: 'James', town: 'London', isDriver: false },
+  { id: 2, name: 'Mike', town: 'Kaunas', isDriver: true },
+  { id: 3, name: 'Bob', town: 'Vilnius', isDriver: false },
+  { id: 4, name: 'Jane', town: 'Klaipeda', isDriver: true },
 ];
 
 // Middleware
@@ -24,8 +24,18 @@ app.get('/', (req, res) => {
 
 // GET - /api/users - grazinti visus userius
 app.get('/api/users', (request, response) => {
-  response.json(users);
+  response.status(200).json(users);
 });
+
+// GET - /api/users/drivers - grazins visus vairuotojus
+
+// GET - /api/users/town - grazinsi visus miestus masyvo pavidalu
+app.get('/api/users/town', (request, response) => {
+  const townsArr = users.map((uObj) => uObj.town);
+  console.log('townsArr ===', townsArr);
+  response.json(townsArr);
+});
+
 // GET - /api/users/1 - grazinti konretu  useri
 // :userId dinamine dalis
 app.get('/api/users/:userId', (request, response) => {
@@ -34,9 +44,24 @@ app.get('/api/users/:userId', (request, response) => {
   // surasti objekta su id === userId ir ji grazinti
   const found = users.find((userObj) => userId === userObj.id);
   console.log('found ===', found);
+  // TODO: not found case
+  // jei neradom
+  if (found === undefined) {
+    response.status(404).json({ msg: `user with id ${userId} was not found` });
+    return;
+  }
   response.json(found);
 });
 
+//  DELETE - /api/users/2 - delete user with id
+app.delete('/api/users/:userId', (request, response) => {
+  const userId = +request.params.userId;
+  // grazlinti viska iskryrus ta el kurio id yra = userId
+  users = users.filter((uObj) => uObj.id !== userId);
+  console.log('users ===', users);
+  response.json(users);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`server is running http://localhost:${port}`);
 });
