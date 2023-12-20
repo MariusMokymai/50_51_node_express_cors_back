af07ca1fc5376f40655c3a83ae5dc087b3ef67f7
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-20 15:28:23 +0200

    dotEnv support

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
+require('dotenv').config(); // ikels .env reiksmes i process.env
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+console.log('process.env.PASS ===', process.env.PASS);
 
 let users = [
   {
