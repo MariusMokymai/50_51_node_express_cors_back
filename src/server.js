f39b2858fda7f64be1ee26773ad323a52407d3ee
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-20 15:32:55 +0200

    uzd

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log('process.env.PASS ===', process.env.PASS);
+// console.log('process.env.PASS ===', process.env.PASS);
 
 let users = [
   {
@@ -35,6 +35,47 @@ let users = [
     isDriver: true,
   },
 ];
+
+let books = [
+  {
+    title: 'Book 1',
+    author: 'Author 1',
+    isPublished: true,
+    year: 2021,
+  },
+  {
+    title: 'Book 2',
+    author: 'Author 2',
+    isPublished: false,
+    year: 2022,
+  },
+  {
+    title: 'Book 3',
+    author: 'Author 3',
+    isPublished: true,
+    year: 2023,
+  },
+  {
+    title: 'Book 4',
+    author: 'Author 4',
+    isPublished: false,
+    year: 2024,
+  },
+  {
+    title: 'Book 5',
+    author: 'Author 5',
+    isPublished: true,
+    year: 2025,
+  },
+];
+
+// crud
+
+// gauti visasa knygas
+// gauti knyga
+// istrinti
+// sukurti
+// atnaujinti
 
 // Middleware
 app.use(morgan('dev'));
