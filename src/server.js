1beb20cafbb916af9855566e0f648a719035d9ad
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-21 13:22:02 +0200

    books routes

--- src/server.js
+++ src/server.js
@@ -35,33 +35,38 @@ let users = [
     isDriver: true,
   },
 ];
-
+// books need  to have id
 let books = [
   {
+    id: 1,
     title: 'Book 1',
     author: 'Author 1',
     isPublished: true,
     year: 2021,
   },
   {
+    id: 2,
     title: 'Book 2',
     author: 'Author 2',
     isPublished: false,
     year: 2022,
   },
   {
+    id: 3,
     title: 'Book 3',
     author: 'Author 3',
     isPublished: true,
     year: 2023,
   },
   {
+    id: 4,
     title: 'Book 4',
     author: 'Author 4',
     isPublished: false,
     year: 2024,
   },
   {
+    id: 5,
     title: 'Book 5',
     author: 'Author 5',
     isPublished: true,
@@ -72,9 +77,40 @@ let books = [
 // crud
 
 // gauti visasa knygas
-// gauti knyga
+app.get('/api/books', (req, res) => {
+  res.json(books);
+});
+// gauti viena knyga
+app.get('/api/books/:bookId', (req, res) => {
+  const bookId = +req.params.bookId;
+  const found = books.find((bookObj) => bookObj.id === bookId);
+  if (found === undefined) {
+    res.status(404).json({
+      msg: `book with id ${bookId} was not found`,
+    });
+    return;
+  }
+  res.json(found);
+});
 // istrinti
+app.delete('/api/books/:bookId', (req, res) => {
+  const bookId = +req.params.bookId;
+  books = books.filter((bookObj) => bookObj.id !== bookId);
+  res.json(books);
+});
 // sukurti
+app.post('/api/books', (req, res) => {
+  const { title, author, isPublished, year } = req.body;
+  const newBook = {
+    id: +Math.random().toString().slice(3),
+    title,
+    author,
+    isPublished,
+    year,
+  };
+  books.push(newBook);
+  res.sendStatus(201);
+});
 // atnaujinti
 
 // Middleware
